d501463defaf061f888f39c3e80680f672991cc8
Cory Grundy <60462387+Virologic1717@users.noreply.github.com>
2020-02-19 10:46:06 -0800

    Add file via upload
    New files uploaded for assignment.

---
+++ JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+window.alert("Hello World!")
+document.write("Hello World!")
+var A = "This is a string";
+document.write(A)
+document.write("So I said\, \"And that is why we can't have any pineapples at the house anymore\!\" While laughing my butt off.")
+function My_First_Function() {
+    var dtring="This text is green!";
+    var result= str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML=result;
+}
