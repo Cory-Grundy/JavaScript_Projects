957b115f6fa185d47bb2fc1f2462930c4054e99f
Cory Grundy <60462387+Virologic1717@users.noreply.github.com>
2020-02-27 12:05:17 -0800

    Add changes to files

---
+++ Basic_HTML_and_JavaScript_1/Basic_Javascript_2.js
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	function My_First_Function(){
+		var String = "Liss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElemenById("Irish").innerHTML = result;
+	}
+</html>
