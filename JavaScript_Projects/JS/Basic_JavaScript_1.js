820c4a09e0f2e2081c7763ef91a8639f0ebea58d
Cory Grundy <60462387+Virologic1717@users.noreply.github.com>
2020-09-28 10:05:06 -0700

    Add file changes via upload

--- JavaScript_Projects/JS/Basic_JavaScript_1.js
+++ JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -4,7 +4,7 @@ var A = "This is a string";
 document.write(A)
 document.write("So I said\, \"And that is why we can't have any pineapples at the house anymore\!\" While laughing my butt off.")
 function My_First_Function() {
-    var string="This text is green!";
+    var dtring="This text is green!";
     var result= str.fontcolor("green");
     document.getElementById("Green_Text").innerHTML=result;
 }
