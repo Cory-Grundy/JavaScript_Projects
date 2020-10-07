58404c2c49c036de8e9ff0ed464ef3dc80089dd3
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2020-10-07 14:17:11 -0700

    Drill 116 revisions

--- JavaScript_Projects/JS/Basic_JavaScript_5.js
+++ JavaScript_Projects/JS/Basic_JavaScript_5.js
@@ -13,11 +13,11 @@ function my_function3() {
 }
 //This functionwill see if there are numbers in the function, then display false, becasue it is all letters.
 function my_function4() {
-    document.getElementById("Test4").innerHTML = document.write(2E310);
+    document.getElementById("Test4").innerHTML = 2E310;
 }
 //This function will display the highest possible floating point number.  It also breaks the webpage, probably because of how large the number is before saying Infinity.
 function my_function5() {
-document.getElementById("Test5").innerHTML = document.write(-3E310);
+document.getElementById("Test5").innerHTML = -3E310;
 }
 //This function will display the lowest possible number floating point number.  This one also breaks the webpage, will need to see how to fix it.
 function my_function6() {
@@ -29,10 +29,11 @@ document.getElementById("Test7").innerHTML = !(10 < 5);
 }
 //This function will also use the NOT operator to display false.
 
-break
 document.write(5>2 && 10>4);
-break
+
 document.write(5>10 || 10>4);
+
+
 
 //Extra code, that I do not recall why it is here. (Notes?)
 // document.write(10 > 2);
@@ -44,18 +45,19 @@ document.write(5>10 || 10>4);
 // document.write("10" + 5);
 
 // console.log(23 < 6);
-break
+
 document.write(10 == 10);
 
 document.write(3 == 11);
-break
+
 x = 4;
 y = 4;
 document.write(x === y);
-break
+
 a = 34;
 b = "cats";
-document.write(a === b);
+c = a + b;
+document.write(c);
 
 // c = "42";
 // d = 42;
