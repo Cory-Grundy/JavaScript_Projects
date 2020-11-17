fa44587c5df8926edf89321de8d312287ea0e267
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2020-11-17 14:51:56 -0800

    Update to files
    Updating files for help from instructor.

--- JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -7,3 +7,43 @@ function Call_Loop() {
     }
     document.getElementById("Loop").innerHTML = Digit;
 }
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function array_Function() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Array").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
+}
+
+function constant_function() {
+    const Mucical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$950";
+    Musical_Instrument.strings = "a five pack of strings";
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price + Musical_Instrument.strings + " for free.";
+}
+
+var X = 82;
+document.write("<br>" + X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+function retun_function() {
+    return Math.PT;
+}
+document.getElementById("Return_Function");
